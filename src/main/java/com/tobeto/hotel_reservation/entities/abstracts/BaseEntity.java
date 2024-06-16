bda071a96b021ec065f38021cf979c07d24d16cf
Sinan <sinandeemir@gmail.com>
2024-06-16 16:23:53 +0300

    added: base entity class

---
+++ src/main/java/com/tobeto/hotel_reservation/entities/abstracts/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.tobeto.hotel_reservation.entities.abstracts;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+}
