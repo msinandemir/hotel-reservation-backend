c716d259256f5c78e2966fb64b6cd85d730b74c3
Sinan <sinandeemir@gmail.com>
2024-06-16 16:30:01 +0300

    added: user entity class

---
+++ src/main/java/com/tobeto/hotel_reservation/entities/concretes/User.java
@@ -0,0 +1,31 @@
+package com.tobeto.hotel_reservation.entities.concretes;
+
+import com.tobeto.hotel_reservation.entities.abstracts.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "users")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User extends BaseEntity {
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
