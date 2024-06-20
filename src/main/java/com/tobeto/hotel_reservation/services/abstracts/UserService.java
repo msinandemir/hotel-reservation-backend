fefca764eb0bd394d803dfa186064044a438f531
Sinan <sinandeemir@gmail.com>
2024-06-20 23:35:36 +0300

    updated: added user crud methods

--- src/main/java/com/tobeto/hotel_reservation/services/abstracts/UserService.java
+++ src/main/java/com/tobeto/hotel_reservation/services/abstracts/UserService.java
@@ -1,4 +1,17 @@
 package com.tobeto.hotel_reservation.services.abstracts;
 
+import com.tobeto.hotel_reservation.services.dtos.user.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+
 public interface UserService {
+    Page<GetUserResponse> getAllUsersWithPagination(int pageNumber, int pageSize,  Sort.Direction sortDirection);
+
+    GetUserResponse getUserById(Long userId, String language);
+
+    AddUserResponse addUser(AddUserRequest request);
+
+    UpdateUserResponse updateUserById(Long userId, UpdateUserRequest request, String language);
+
+    void deleteUserById(Long userId, String language);
 }
--- src/main/java/com/tobeto/hotel_reservation/services/concretes/UserServiceImpl.java
+++ src/main/java/com/tobeto/hotel_reservation/services/concretes/UserServiceImpl.java
@@ -1,6 +1,72 @@
 package com.tobeto.hotel_reservation.services.concretes;
 
+import com.tobeto.hotel_reservation.core.exceptions.types.BusinessException;
+import com.tobeto.hotel_reservation.entities.concretes.User;
+import com.tobeto.hotel_reservation.repositories.UserRepository;
 import com.tobeto.hotel_reservation.services.abstracts.UserService;
+import com.tobeto.hotel_reservation.services.dtos.user.*;
+import com.tobeto.hotel_reservation.services.mappers.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Cacheable(cacheNames = "users", key = "#root.methodName", unless = "#result == null")
+    @Override
+    public Page<GetUserResponse> getAllUsersWithPagination(int pageNumber, int pageSize, Sort.Direction sortDirection) {
+        Sort sorting = Sort.by(sortDirection, "createdAt");
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, sorting);
+        Page<User> users = userRepository.findAll(pageable);
+
+        List<GetUserResponse> responses = users.stream()
+                .map(UserMapper.INSTANCE::getResponseFromUser)
+                .toList();
+        return new PageImpl<>(responses, pageable, users.getTotalElements());
+    }
+
+    @Cacheable(cacheNames = "user_id", key = "'#root.methodName_' + userId", unless = "#result == null")
+    @Override
+    public GetUserResponse getUserById(Long userId, String language) {
+        User foundUser = findUserById(userId, language);
+        return UserMapper.INSTANCE.getResponseFromUser(foundUser);
+    }
+
+    @CacheEvict(cacheNames = {"user_id", "users"}, allEntries = true)
+    @Override
+    public AddUserResponse addUser(AddUserRequest request) {
+        User user = UserMapper.INSTANCE.userFromAddRequest(request);
+        User savedUser = userRepository.save(user);
+        return UserMapper.INSTANCE.addResponseFromUser(savedUser);
+    }
+
+    @CachePut(cacheNames = "user_id", key = "'getUserById_' + #request.id", unless = "#result == null")
+    @Override
+    public UpdateUserResponse updateUserById(Long userId, UpdateUserRequest request, String language) {
+        User foundUser = findUserById(userId, language);
+        User updatedUser = UserMapper.INSTANCE.userFromUpdateRequest(request);
+        updatedUser.setId(foundUser.getId());
+
+        User savedUser = userRepository.save(updatedUser);
+        return UserMapper.INSTANCE.updateResponseFromUser(savedUser);
+    }
+
+    @CacheEvict(cacheNames = {"user_id", "users"}, allEntries = true)
+    @Override
+    public void deleteUserById(Long userId, String language) {
+        User foundUser = findUserById(userId, language);
+        userRepository.deleteById(foundUser.getId());
+    }
+
+    private User findUserById(Long userId, String language) {
+        return userRepository.findById(userId).orElseThrow(() -> new BusinessException("error.userNotFound", language));
+    }
 }
