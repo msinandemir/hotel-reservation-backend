251ce2adf71ee45b7813f25880d5abe776977a46
Sinan <sinandeemir@gmail.com>
2024-06-22 23:16:04 +0300

    updated: missed fields added to hotel's dtos

--- src/main/java/com/tobeto/hotel_reservation/services/dtos/hotel/AddHotelResponse.java
+++ src/main/java/com/tobeto/hotel_reservation/services/dtos/hotel/AddHotelResponse.java
@@ -5,11 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.Instant;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class AddHotelResponse {
+    private Long id;
+    private Instant createdAt;
+    private Instant updatedAt;
     private String name;
     private int star;
     private String phoneNumber;
