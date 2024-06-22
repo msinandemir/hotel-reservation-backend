1c5a4d2f6d5730a397851640a80980e0967be6f8
Sinan <sinandeemir@gmail.com>
2024-06-22 23:18:43 +0300

    updated: missed fields added to room's dtos

--- src/main/java/com/tobeto/hotel_reservation/services/dtos/room/GetRoomResponse.java
+++ src/main/java/com/tobeto/hotel_reservation/services/dtos/room/GetRoomResponse.java
@@ -7,12 +7,16 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class GetRoomResponse {
+    private Long id;
+    private Instant createdAt;
+    private Instant updatedAt;
     private int capacity;
     private BigDecimal price;
     private boolean availability;
