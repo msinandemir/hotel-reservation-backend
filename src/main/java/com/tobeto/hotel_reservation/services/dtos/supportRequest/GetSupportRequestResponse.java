1203ed6a33db2949b0f1c5d6638e7b4e3e2ebafc
Sinan <sinandeemir@gmail.com>
2024-06-22 23:19:40 +0300

    updated: missed fields added to support request's dtos

--- src/main/java/com/tobeto/hotel_reservation/services/dtos/supportRequest/GetSupportRequestResponse.java
+++ src/main/java/com/tobeto/hotel_reservation/services/dtos/supportRequest/GetSupportRequestResponse.java
@@ -6,11 +6,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.Instant;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class GetSupportRequestResponse {
+    private Long id;
+    private Instant createdAt;
+    private Instant updatedAt;
     private String title;
     private String description;
     private SupportRequestType type;
