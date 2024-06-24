9ab4e2ccaf7ab93056496b49262221dc833397ed
Sinan <sinandeemir@gmail.com>
2024-06-24 17:38:43 +0300

    added: hotel info mapper

---
+++ src/main/java/com/tobeto/hotel_reservation/services/mappers/HotelInfoMapper.java
@@ -0,0 +1,27 @@
+package com.tobeto.hotel_reservation.services.mappers;
+
+import com.tobeto.hotel_reservation.entities.concretes.HotelInfo;
+import com.tobeto.hotel_reservation.services.dtos.hotelInfo.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface HotelInfoMapper {
+    HotelInfoMapper INSTANCE = Mappers.getMapper(HotelInfoMapper.class);
+
+    @Mapping(source = "hotel.id", target = "hotel.id")
+    GetHotelInfoResponse getResponseFromHotelInfo(HotelInfo hotelInfo);
+
+    @Mapping(target = "hotel.id", source = "hotelId")
+    HotelInfo hotelInfoFromAddRequest(AddHotelInfoRequest request);
+
+    @Mapping(source = "hotel.id", target = "hotelId")
+    AddHotelInfoResponse addResponseFromHotelInfo(HotelInfo hotelInfo);
+
+    @Mapping(target = "hotel.id", source = "hotelId")
+    HotelInfo hotelInfoFromUpdateRequest(UpdateHotelInfoRequest request);
+
+    @Mapping(source = "hotel.id", target = "hotelId")
+    UpdateHotelInfoResponse updateResponseFromHotelInfo(HotelInfo hotelInfo);
+}
