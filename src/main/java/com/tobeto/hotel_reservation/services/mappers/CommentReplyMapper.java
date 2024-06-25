3b4a228205eaf002f03212422bc70ebef02c29d1
Sinan <sinandeemir@gmail.com>
2024-06-25 19:32:09 +0300

    added: comment reply mapper

---
+++ src/main/java/com/tobeto/hotel_reservation/services/mappers/CommentReplyMapper.java
@@ -0,0 +1,32 @@
+package com.tobeto.hotel_reservation.services.mappers;
+
+import com.tobeto.hotel_reservation.entities.concretes.CommentReply;
+import com.tobeto.hotel_reservation.services.dtos.commentReply.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CommentReplyMapper {
+    CommentReplyMapper INSTANCE = Mappers.getMapper(CommentReplyMapper.class);
+
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "comment.id", target = "userId")
+    GetCommentReplyResponse getResponseFromCommentReply(CommentReply commentReply);
+
+    @Mapping(target = "user.id", source = "userId")
+    @Mapping(target = "comment.id", source = "commentId")
+    CommentReply commentReplyFromAddRequest(AddCommentReplyRequest request);
+
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "comment.id", target = "userId")
+    AddCommentReplyResponse addResponseFromCommentReply(CommentReply commentReply);
+
+    @Mapping(target = "user.id", source = "userId")
+    @Mapping(target = "comment.id", source = "commentId")
+    CommentReply commentReplyFromUpdateRequest(UpdateCommentReplyRequest request);
+
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "comment.id", target = "userId")
+    UpdateCommentReplyResponse updateResponseFromCommentReply(CommentReply commentReply);
+}
