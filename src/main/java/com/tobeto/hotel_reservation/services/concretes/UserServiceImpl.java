94bed6ca648f398a135cd660bb9e262669afeb11
Sinan <sinandeemir@gmail.com>
2024-07-08 17:38:42 +0300

    refactored: cacheput key fixed

--- src/main/java/com/tobeto/hotel_reservation/services/concretes/UserServiceImpl.java
+++ src/main/java/com/tobeto/hotel_reservation/services/concretes/UserServiceImpl.java
@@ -53,7 +53,7 @@ public class UserServiceImpl implements UserService {
         return UserMapper.INSTANCE.addResponseFromUser(savedUser);
     }
 
-    @CachePut(cacheNames = "user_id", key = "'getUserById' + #request.id", unless = "#result == null")
+    @CachePut(cacheNames = "user_id", key = "'getUserById' + #userId", unless = "#result == null")
     @Override
     public UpdateUserResponse updateUserById(Long userId, UpdateUserRequest request, String language) {
         User foundUser = findUserById(userId, language);
